6190684a551a3a18b7740d9aa2a5add06e9bf931
lfontLS <lien.font@lifesurge.com>
2026-03-11 23:32:54 -0400

    adjusted some of the light/dark theme coloring | added the prisma lib to connect ot a future PostgreSQL DB

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	const styles = {
 		container:
-			'flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans bg-linear-65 from-purple-500 to-pink-500 dark:from-purple-900 dark:to-pink-900 dark:bg-black gap-2',
+			'flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans bg-linear-65 from-purple-400 to-pink-300 dark:from-purple-900 dark:to-pink-900 gap-2',
 	};
 
 	return <div className={styles.container}></div>;
